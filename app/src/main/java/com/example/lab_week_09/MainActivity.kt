e69ee387e99e2b72e6815d89d0953b493931221b
michaeltamba <michaelltambaa@gmail.com>
2025-11-10 17:39:06 +0700

    Commit No. 4 – Navigation

--- app/src/main/java/com/example/lab_week_09/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_09/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -27,6 +28,13 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.lab_week_09.ui.theme.LAB_WEEK_09Theme
 import com.example.lab_week_09.ui.theme.OnBackgroundItemText
 import com.example.lab_week_09.ui.theme.OnBackgroundTitleText
@@ -42,7 +50,8 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
-                    Home()
+                    val navController = rememberNavController()
+                    App(navController = navController)
                 }
             }
         }
@@ -53,9 +62,21 @@ data class Student(
     var name: String
 )
 
+@Composable
+fun App(navController: NavHostController){
+    NavHost(navController = navController, startDestination = "home"){
+        composable("home"){
+            Home{navController.navigate("resultContent/?listData=$it")}
+        }
+
+        composable("resultContent/?listData={listData}", arguments = listOf(navArgument("listData"){type = NavType.StringType})) {
+            ResultContent(it.arguments?.getString("listData").orEmpty())
+        }
+    }
+}
 
 @Composable
-fun Home() {
+fun Home(navigateFromHomeToResult: (String) -> Unit) {
     val listData = remember { mutableStateListOf(Student("Tanu"), Student("Tina"), Student("Tono")) }
     var inputField = remember { mutableStateOf(Student("")) }
 
@@ -65,7 +86,7 @@ fun Home() {
                 listData.add(inputField.value)
                 inputField.value = Student("")
             }
-        })
+        }, {navigateFromHomeToResult(listData.toList().toString())})
 }
 
 @Composable
@@ -73,15 +94,21 @@ fun HomeContent(
     listData: SnapshotStateList<Student>,
     inputField: Student,
     onInputValueChange: (String) -> Unit,
-    onButtonClick: () -> Unit
+    onButtonClick: () -> Unit,
+    navigateFromHomeToResult: () -> Unit
 ) {
     LazyColumn {
         item {
             Column(modifier = Modifier.padding(16.dp).fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
                 OnBackgroundTitleText(text = stringResource(id = R.string.enter_item))
                 TextField(value = inputField.name, keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text), onValueChange = {onInputValueChange(it)})
-                PrimaryTextButton(text = stringResource(id = R.string.button_click)) {
-                    onButtonClick()
+                Row {
+                    PrimaryTextButton(text = stringResource(id = R.string.button_click)) {
+                        onButtonClick()
+                    }
+                    PrimaryTextButton(text = stringResource(id = R.string.button_navigate)) {
+                        navigateFromHomeToResult()
+                    }
                 }
             }
         }
@@ -96,5 +123,13 @@ fun HomeContent(
 @Preview(showBackground = true)
 @Composable
 fun PreviewHome() {
-    Home()
+    val navController = rememberNavController()
+    App(navController = navController)
+}
+
+@Composable
+fun ResultContent(listData: String){
+    Column(modifier = Modifier.padding(vertical = 4.dp).fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
+        OnBackgroundItemText(text = listData)
+    }
 }
